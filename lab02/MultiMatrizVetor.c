5824aea7b3627232826d40303be2f8d2b0fc1d26
Cloud User <ec2-user@ip-172-31-18-183.ec2.internal>
2024-03-18 20:48:19 +0000

    Adicionando MultiMatrizVetor.c corrigido

--- lab02/MultiMatrizVetor.c
+++ lab02/MultiMatrizVetor.c
@@ -2,10 +2,11 @@
 #include <pthread.h>
 
 //Definir matriz
-int A[3][3] = {
+int A[4][3] = {
 {2,4,6},
 {1,2,3},
-{5,7,9}
+{5,7,9},
+{1,1,1}
 };
 
 //Definir vetores
@@ -13,7 +14,8 @@ int vet[3] = {1,2,3}; //Vetor que será multiplicado pela matriz
 int result[3] = {0,0,0}; //Vetor que guardará os resultados
 
 //Tamanho do vetor
-int tamanho = sizeof(vet)/sizeof(vet[0]);
+int qnt_linhas = sizeof(A)/sizeof(A[0]);
+int tamanho_vet = sizeof(vet)/sizeof(vet[0]);
 
 //Função que imprime o vetor A para visualização da operação
 void printaVetorThread(int *A, int len){
@@ -26,16 +28,16 @@ void printaVetorThread(int *A, int len){
 void* calcula_linha(void* args){
   int *index = (int*)args;
   int i = *index;
-  for(int j = 0; j < tamanho; j++){
+  for(int j = 0; j < tamanho_vet; j++){
     result[i] += A[i][j] * vet[j];
   }
-  printaVetorThread(result, tamanho);
+  printaVetorThread(result, qnt_linhas);
 }
 
 int main(void) {
   //Criar pthreads
-  pthread_t threads[tamanho];
-  for(int i = 0; i < tamanho; i++){
+  pthread_t threads[qnt_linhas];
+  for(int i = 0; i < qnt_linhas; i++){
     //Verifica se a criação das threads foi bem sucedida
     if(pthread_create(&threads[i], NULL, calcula_linha, &i) != 0){
       printf("Erro ao criar threads");
@@ -49,8 +51,8 @@ int main(void) {
   //Printa resultado
   printf("Vetor resultado: ");
   printf("[");
-  for(int i = 0; i < tamanho; i++){
-    if(i == tamanho-1){
+  for(int i = 0; i < qnt_linhas; i++){
+    if(i == qnt_linhas-1){
       printf("%d", result[i]);
     }
     else{
